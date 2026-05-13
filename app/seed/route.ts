e524cc611d41664605852d8db1189751ee26b74c
marsil1990 <ma.silveram@gmail.com>
2026-05-13 10:41:08 -0300

    Agregar cambios

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -5,7 +5,7 @@ import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  //await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -103,12 +103,13 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;//nuevo
+    // const result = await sql.begin((sql) => [
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
+    // ]);
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
