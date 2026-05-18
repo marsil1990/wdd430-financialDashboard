5f2f942e42f0538f50f659d4e31a8532adb9e3ca
marsil1990 <ma.silveram@gmail.com>
2026-05-18 13:09:03 -0300

    week3

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -20,8 +20,8 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
   }) {
   const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useActionState(updateInvoice, initialState);
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   
 
   return (
