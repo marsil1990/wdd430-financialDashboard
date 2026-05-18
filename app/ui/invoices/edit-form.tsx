64d388854ae0aae115a6e18f31c0004acc150c73
marsil1990 <ma.silveram@gmail.com>
2026-05-18 13:12:56 -0300

    week3

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -73,7 +73,7 @@ export default function EditInvoiceForm({
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
         {state.errors?.amount &&
           state.errors.amount.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
@@ -127,7 +127,7 @@ export default function EditInvoiceForm({
               </div>
               
             </div>
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
+            <div id="status-error" aria-live="polite" aria-atomic="true">
         {state.errors?.status &&
           state.errors.status.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
